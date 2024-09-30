198e2a65a0775015247319f0a0c12b7b5a1514dc
ABMBE <ermacsebas@gmail.com>
2024-09-29 22:20:44 -0500

    slowly but surely

---
+++ finalcss/src/Contact/page1.tsx
@@ -0,0 +1,14 @@
+import Imagen from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <main>
+                <div>
+                    <h1>PRIMERA PARTE DE LA PAGINA</h1>
+                </div>
+            </main>
+        </div>
+    );
+}
+
